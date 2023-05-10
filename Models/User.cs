8280dcdf825bc4f5f2af37fb73307d5b611c3cf9
GruiaAnton <vitan.alexandra.i7i@student.ucv.ro>
2023-05-10 16:05:13 +0300

    Added Login & Register

--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace ArtClub.Models
 {
     public class User : ModelEntity
     {
-
+   
         public string Role { get; set; }   = string.Empty;
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
