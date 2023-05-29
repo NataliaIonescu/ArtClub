d02ba666935e779940c26892f030b3294c11addf
natalia9 <nataliaionescu5@gmail.com>
2023-05-29 10:51:45 +0300

    update

--- Tests/ArtClubTests/EventsControllerTestsMock.cs
+++ Tests/ArtClubTests/EventsControllerTestsMock.cs
@@ -2,7 +2,6 @@ using ArtClub.Controllers;
 using ArtClub.Models;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
-using Moq.Language.Flow;
 using Microsoft.EntityFrameworkCore;
 namespace ArtClubTests
 {
@@ -75,46 +74,6 @@ namespace ArtClubTests
             Assert.IsTrue(result.ViewData.ModelState.ContainsKey(""));
             Assert.AreEqual("An event with the same StartDate, EndDate and ResourceId already exists.", result.ViewData.ModelState[""].Errors[0].ErrorMessage);
         }
-  
-        [TestMethod]
-        public async Task Edit_ValidEvent_ReturnsRedirectToIndex()
-        {
-            // Arrange
-            var existingEvent = new Event
-            {
-                Id = 1,
-                StartDate = new DateTime(2023, 6, 1),
-                EndDate = new DateTime(2023, 6, 3),
-                Description = "Test event",
-                ResourceId = 1
-            };
-            _mockContext.Setup(c => c.Events.FindAsync(existingEvent.Id))
-                .ReturnsAsync(existingEvent);
-
-            var updatedEvent = new Event
-            {
-                Id = 1,
-                StartDate = new DateTime(2023, 6, 4),
-                EndDate = new DateTime(2023, 6, 6),
-                Description = "Updated event",
-                ResourceId = 1
-            };
-
-            _mockContext.Setup(c => c.Events.AnyAsync(e =>
-                e.StartDate == updatedEvent.StartDate &&
-                e.EndDate == updatedEvent.EndDate &&
-                e.ResourceId == updatedEvent.ResourceId &&
-                e.Id != updatedEvent.Id,
-                It.IsAny<CancellationToken>())).ReturnsAsync(false);
-
-            // Act
-            var result = await _controller.Edit(updatedEvent.Id, updatedEvent) as RedirectToActionResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual("Index", result.ActionName);
-        }
-
 
         [TestMethod]
         public async Task Delete_ValidEvent_ReturnsRedirectToIndex()
@@ -155,7 +114,44 @@ namespace ArtClubTests
         }
 
 
+        [TestMethod]
+        public async Task Edit_ValidEvent_ReturnsRedirectToIndex()
+        {
+            // Arrange
+            var existingEvent = new Event
+            {
+                Id = 1,
+                StartDate = new DateTime(2023, 6, 1),
+                EndDate = new DateTime(2023, 6, 3),
+                Description = "Test event",
+                ResourceId = 1
+            };
+            _mockContext.Setup(c => c.Events.FindAsync(existingEvent.Id))
+                .ReturnsAsync(existingEvent);
 
+            var updatedEvent = new Event
+            {
+                Id = 1,
+                StartDate = new DateTime(2023, 6, 4),
+                EndDate = new DateTime(2023, 6, 6),
+                Description = "Updated event",
+                ResourceId = 1
+            };
+
+            _mockContext.Setup(c => c.Events.AnyAsync(e =>
+                e.StartDate == updatedEvent.StartDate &&
+                e.EndDate == updatedEvent.EndDate &&
+                e.ResourceId == updatedEvent.ResourceId &&
+                e.Id != updatedEvent.Id,
+                It.IsAny<CancellationToken>())).ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.Edit(updatedEvent.Id, updatedEvent) as RedirectToActionResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.ActionName);
+        }
 
     }
 }
---
+++ Tests/ArtClubTests/LoginPageTests.cs
@@ -0,0 +1,54 @@
+﻿
+using Microsoft.Identity.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+
+namespace ArtClubTests
+{
+    [TestClass]
+    public class LoginPageTests
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            // driver.Navigate().GoToUrl("URL_OF_LOGIN_PAGE");
+            driver.Navigate().GoToUrl("https://localhost:7036/Identity/Account/Login");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void TestLoginPage()
+        {
+            // Fill in the login form fields
+            var emailInput = driver.FindElement(By.Id("Input_Email"));
+            emailInput.SendKeys("john_Admin1@gmail.com");
+
+            var passwordInput = driver.FindElement(By.Id("Input_Password"));
+            passwordInput.SendKeys("john_Admin1@gmail.com");
+
+            var loginButton = driver.FindElement(By.Id("login-submit"));
+            loginButton.Click();
+
+            // Wait for the page to navigate or for an element on the next page to appear
+           
+           var nextPageElement = wait.Until(ExpectedConditions.ElementExists(By.Id("https://localhost:7036/")));
+            // Assert any necessary conditions on the next page
+            Assert.IsNotNull(nextPageElement);
+           
+        }
+    }
+}
